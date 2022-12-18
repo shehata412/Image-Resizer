75f42f0a1e691e8bb2769aa014e98baf1ab84be0
shehata412 <shehata412@aucegypt.edu>
2022-12-18 12:41:51 +0200

    all functionality done remains the testing and some checks

--- src/routes/api/resize.ts
+++ src/routes/api/resize.ts
@@ -1,26 +1,27 @@
-import path from 'path'
-import express from 'express'
-import sharp from 'sharp'
+import path from 'path';
+import express from 'express';
+import sharp from 'sharp';
 
 const resize = express.Router();
-
-const img = path.join(__dirname, 'panda.jpeg');
-
-resize.get('/resize',async (req: express.Request, res: express.Response) => {
-    if(Object.keys(req.query).length !== 0){
-    const height:number = parseInt(req.query.height as string)
-    const width:number = parseInt(req.query.width as string)
-    res.send('I am here');
-try{
-    await sharp(img).resize(height,width).toFile('output.jpeg');
-}catch(err) {
-    console.log(err);
-}
+const img_path: string = path.join(__dirname, '../../../assets/full/');
+const new_img_path: string = path.join(__dirname, '../../../assets/thumb/');
+resize.get('/resize', async (req: express.Request, res: express.Response) => {
+  if (Object.keys(req.query).length !== 0) {
+    const img: string = img_path + `${req.query.filename as string}.jpeg`;
+    const height: number = parseInt(req.query.height as string);
+    const width: number = parseInt(req.query.width as string);
+    try {
+      await sharp(img)
+        .resize(height, width)
+        .toFile(new_img_path + `${req.query.filename as string}_thumb.jpeg`);
+      res.sendFile(new_img_path + `${req.query.filename as string}_thumb.jpeg`);
+    } catch (err) {
+      console.log(err);
+      res.send(`An error has occurred ${err}`);
     }
-    else{
+  } else {
     res.send('Bad request please enter the correct parameters');
-    }
+  }
 });
-
 
 export = resize;
